ecc3f9236a5895feeda5635e3662c92289f78070
EC2 Default User <ec2-user@ip-10-16-148-101.us-west-2.compute.internal>
2014-09-28 07:45:18 +0000

    server

---
+++ test/controllers/raises_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RaisesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/raises_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class RaisesHelperTest < ActionView::TestCase
+end
